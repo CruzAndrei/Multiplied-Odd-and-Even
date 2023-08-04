12335bc9e0b00c36ee786c8e245b566e2c8a2330
CruzAndrei <cruzandrei237@gmail.com>
2023-08-04 12:02:55 +0800

    text files

---
+++ Multipled Odd and Even.py
@@ -0,0 +1,11 @@
+#write a method in python that will create two separate text files after reading the source text file named integers.txt that contains 20 integers. 
+#The first output file will be named double.txt containing the square of all even integers found in intergers.txt 
+#and the second file will be named triple.txt containing the cube of all odd numbers found in the integers.txt
+#open the three text files
+#read the numbers.txt that contains 20integers in each line    
+#if the numbers are even
+#square the numbers
+#write it in the double.txt
+#if the numbers are odd
+#cubed the numbers
+#write it in the triple.txt
